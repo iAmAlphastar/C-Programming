4414c45fb7bb7de48b2e16aaad81bf18f2fda541
Rohan Satyamurthy <rohan.satyamurthy@gmail.com>
2026-09-19 01:13:54 +0530

    adding exercise
    additonal exercise

---
+++ Learning_C_Programming_Byron-Gottfried/01_Chapter/Ex_02_Integer_Variable.c
@@ -0,0 +1,15 @@
+/* Exercise 2 — Integer Variables */
+#include <stdio.h>
+
+int main()
+{
+    int age = 32;
+    int experience = 7;
+    int projects = 5;
+    
+    printf("age = %d\n",age);
+    printf("experience = %d\n",experience);
+    printf("projects = %d\n",projects);
+    
+    return(0);
+}
